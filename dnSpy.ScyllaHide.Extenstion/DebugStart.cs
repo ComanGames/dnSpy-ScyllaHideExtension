01a329be13a182ed43bb102eb206076aa440935e
Yuriy Paramonov <coman.games@outlook.com>
2018-04-20 04:12:06 +0300

    Add Project to source control

---
+++ dnSpy.ScyllaHide.Extenstion/DebugStart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using dnSpy.Contracts.App;
+using dnSpy.Contracts.Debugger;
+using dnSpy.Contracts.Debugger.References;
+
+namespace dnSpy.ScyllaHide {
+	[Export(typeof(IDbgManagerStartListener))]
+	sealed class DebugStart:IDbgManagerStartListener {
+		public static SynchronizationContext main;
+		public static DbgManager dbg;
+		public void OnStart(DbgManager dbgManager) {
+
+			dbg = dbgManager;
+			 main = SynchronizationContext.Current;
+			dbgManager.DelayedIsRunningChanged += (sender, args) => { ShowCountOfProcesses(dbgManager); };
+		}
+
+		private static void ShowCountOfProcesses(DbgManager dbgManager)
+		{
+			if (dbgManager?.IsRunning==true&&dbgManager.Processes.Length>0) {
+				ulong pid = dbgManager.Processes[0].Id;
+				StartScyllaDide(pid);
+				Thread.Sleep(1000);
+			}
+		}
+
+		private static void StartScyllaDide(ulong proccessId) {
+			string scyllaProg = @"C:\MyPograms\ScyllaHide\InjectorCLIx64.exe";
+
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.FileName = scyllaProg;
+			string dll = @"C:\MyPograms\ScyllaHide\HookLibraryx64.dll";	
+			startInfo.Arguments = $"pid:{proccessId} {dll}";
+			startInfo.CreateNoWindow = true;
+			Process.Start(startInfo);
+		}
+	}
+}
