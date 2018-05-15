5dab4bf95f05bcf096c078af7048a3a137df08c1
Yuriy Paramonov <coman.games@outlook.com>
2018-05-15 13:48:16 +0300

    Add options in xaml document.

---
+++ dnSpy.ScyllaHide.Extenstion/MySettingsControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Example1.Extension {
+	public partial class MySettingsControl : UserControl {
+		public MySettingsControl() => InitializeComponent();
+
+        private void CheckBox_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
