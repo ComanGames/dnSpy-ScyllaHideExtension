bbe655abf1cf53d84170561a4557de2fd3bf24f5
Yuriy Paramonov <coman.games@outlook.com>
2018-05-30 20:03:35 +0300

    Add parsing iniFiles project that I was working for a while and recahsed everything

--- Example2.Extension/ToolWindowControl.xaml.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace Example2.Extension {
-	public partial class ToolWindowControl : UserControl {
-		public ToolWindowControl() => InitializeComponent();
-	}
-}
--- dnSpy.ScyllaHide.Extenstion/ScyllaHideSettingsControl.xaml.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace dnSpy.ScyllaHide {
-	public partial class ScyllaHideSettingsControl : UserControl {
-		public ScyllaHideSettingsControl() => InitializeComponent();
-
-    }
-}
